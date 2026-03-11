a2c6081cb3648b39a3c137ebb672ef0e166f5143
Masonwp123 <masonparker868@gmail.com>
2026-03-11 11:35:38 -0400

    Added grid flippable cards

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,184 @@
 <script setup>
+import {ref} from "vue";
+import Card from "primevue/card"
+import FlipCard from "./components/FlipCard.vue"
+
+let cardVar = ref(
+    [
+      { show: false },
+      { show: false },
+      { show: false },
+      { show: false },
+      { show: false },
+      { show: false },
+      { show: false },
+      { show: false }
+    ]
+);
+
 </script>
 
 <template>
-  <div>
-    Hello
+  <div class="grid">
+    <FlipCard :show="cardVar[0]" @click="cardVar[0] = !cardVar[0]">
+      <template #front>
+        <Card class="fill">
+          <template #content>
+            <h2> Card Front </h2>
+          </template>
+        </Card>
+      </template>
+
+      <template #back>
+        <Card class="fill">
+          <template #title>Card Back</template>
+          <template #content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non dictum orci. Aenean et aliquet orci. Nunc tristique orci tortor.
+          </template>
+        </Card>
+      </template>
+    </FlipCard>
+    <FlipCard :show="cardVar[1]" @click="cardVar[1] = !cardVar[1]">
+      <template #front>
+        <Card class="fill">
+          <template #content>
+            <h2> Card Front </h2>
+          </template>
+        </Card>
+      </template>
+
+      <template #back>
+        <Card class="fill">
+          <template #title>Card Back</template>
+          <template #content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non dictum orci. Aenean et aliquet orci. Nunc tristique orci tortor.
+          </template>
+        </Card>
+      </template>
+    </FlipCard>
+    <FlipCard :show="cardVar[2]" @click="cardVar[2] = !cardVar[2]">
+      <template #front>
+        <Card class="fill">
+          <template #content>
+            <h2> Card Front </h2>
+          </template>
+        </Card>
+      </template>
+
+      <template #back>
+        <Card class="fill">
+          <template #title>Card Back</template>
+          <template #content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non dictum orci. Aenean et aliquet orci. Nunc tristique orci tortor.
+          </template>
+        </Card>
+      </template>
+    </FlipCard>
+    <FlipCard :show="cardVar[3]" @click="cardVar[3] = !cardVar[3]">
+      <template #front>
+        <Card class="fill">
+          <template #content>
+            <h2> Card Front </h2>
+          </template>
+        </Card>
+      </template>
+
+      <template #back>
+        <Card class="fill">
+          <template #title>Card Back</template>
+          <template #content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non dictum orci. Aenean et aliquet orci. Nunc tristique orci tortor.
+          </template>
+        </Card>
+      </template>
+    </FlipCard>
+  </div>
+
+  <div class="grid">
+    <FlipCard :show="cardVar[4]" @click="cardVar[4] = !cardVar[4]">
+      <template #front>
+        <Card class="fill">
+          <template #content>
+            <h2> Card Front </h2>
+          </template>
+        </Card>
+      </template>
+
+      <template #back>
+        <Card class="fill">
+          <template #title>Card Back</template>
+          <template #content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non dictum orci. Aenean et aliquet orci. Nunc tristique orci tortor.
+          </template>
+        </Card>
+      </template>
+    </FlipCard>
+    <FlipCard :show="cardVar[5]" @click="cardVar[5] = !cardVar[5]">
+      <template #front>
+        <Card class="fill">
+          <template #content>
+            <h2> Card Front </h2>
+          </template>
+        </Card>
+      </template>
+
+      <template #back>
+        <Card class="fill">
+          <template #title>Card Back</template>
+          <template #content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non dictum orci. Aenean et aliquet orci. Nunc tristique orci tortor.
+          </template>
+        </Card>
+      </template>
+    </FlipCard>
+    <FlipCard :show="cardVar[6]" @click="cardVar[6] = !cardVar[6]">
+      <template #front>
+        <Card class="fill">
+          <template #content>
+            <h2> Card Front </h2>
+          </template>
+        </Card>
+      </template>
+
+      <template #back>
+        <Card class="fill">
+          <template #title>Card Back</template>
+          <template #content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non dictum orci. Aenean et aliquet orci. Nunc tristique orci tortor.
+          </template>
+        </Card>
+      </template>
+    </FlipCard>
+    <FlipCard :show="cardVar[7]" @click="cardVar[7] = !cardVar[7]">
+      <template #front>
+        <Card class="fill">
+          <template #content>
+            <h2> Card Front </h2>
+          </template>
+        </Card>
+      </template>
+
+      <template #back>
+        <Card class="fill">
+          <template #title>Card Back</template>
+          <template #content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non dictum orci. Aenean et aliquet orci. Nunc tristique orci tortor.
+          </template>
+        </Card>
+      </template>
+    </FlipCard>
   </div>
 </template>
 
 <style scoped>
+
+.grid {
+  display : inline-flex;
+}
+
+.fill {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
 </style>
---
+++ src/components/FlipCard.vue
@@ -0,0 +1,46 @@
+<script setup>
+defineProps(['show'])
+</script>
+
+<template>
+  <div class="flip-card">
+    <div class="flip-card-inner" :class="{flipped: show}">
+      <div class="flip-card-slot flipped">
+        <slot name="front"/>
+      </div>
+      <div class="flip-card-slot">
+        <slot name="back"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.flip-card {
+  background-color: transparent;
+  width: 300px;
+  height: 200px;
+  perspective: 1000px;
+}
+
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+
+.flipped {
+  transform: rotateY(180deg);
+}
+
+.flip-card-slot {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+}
+</style>
